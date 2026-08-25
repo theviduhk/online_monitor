fa770d4f079b01743d65b98c9293749967529d33
Thevidu Samal <theviduhk@gmail.com>
2026-08-25 21:06:16 +0530

    Replace 'kiyana' with 'fapharmafr' in index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PROJECTS = [
   "cbcdairyil","inbevci","inbevnl","diageofr","marsbh","marskw","marsom",
   "marsqa","marsuae","risparkwinede","straussdryil","straussil","jtihr",
   "straussfritolayil","tevade","tevapl","bdftr","pngza2","beiersdorfsp","tevaru",
-  "gskua","kraftheinzde","beiersdorfza","kiyana"
+  "gskua","kraftheinzde","beiersdorfza","fapharmafr"
 ];
 
 const METRICS = [
